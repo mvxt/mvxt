d9f1eb9341f6a0e9f62500562e0bed9c22aeadf9
Michael V Thanh <meta.orphic@gmail.com>
2021-02-14 23:44:56 -0700

    Fix issue with pretty paths in s3

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Layout from '../components/layout';
+
+export default function NotFound() {
+  return (
+    <Layout title="404 Not Found">
+      <p>The page you're looking for was not found. If you think this is a mistake, please reach out to me. My contact info is on the <a href="/about">About page</a>.</p>
+    </Layout>
+  );
+};
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,4 @@
 import Layout from '../components/layout';
-import { config } from "@fortawesome/fontawesome-svg-core";
-
-config.autoAddCss = false;
 
 export default function About() {
   return (
