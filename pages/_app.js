8b72f55f54a4b7936e086f45d4e0b14927de834b
Michael Vinh Xuan Thanh <meta.orphic@gmail.com>
2021-08-19 02:13:13 -0600

    Add cosmetic placeholders to downloads, add ko-fi popup

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,29 @@ config.autoAddCss = false;
 export default function Mvxt({ Component, pageProps }) {
   let description = "Michael Vinh Xuan Thanh is a Sr. Sales Engineer, software developer, and musician. Read more about his previous and ongoing projects.";
 
+  const [kofiLoaded, setKofiLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
+    script.async = true;
+    script.defer = true;
+    script.crossOrigin = 'anonymous';
+    script.onload = () => setKofiLoaded(true);
+    document.body.append(script);
+  }, []);
+
+  React.useEffect(() => {
+    if (!kofiLoaded) return;
+
+    window.kofiWidgetOverlay.draw('mvxt1', {
+        'type': 'floating-chat',
+        'floating-chat.donateButton.text': 'Tip Me',
+        'floating-chat.donateButton.background-color': '#64171a',
+        'floating-chat.donateButton.text-color': '#fff'
+      });
+  }, [kofiLoaded]);
+
   return (
     <>
     <Head>
--- pages/downloads.js
+++ pages/downloads.js
@@ -1,12 +1,82 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {
+  faSearch
+} from '@fortawesome/free-solid-svg-icons';
+
 import Layout from '../components/layout';
 
-export default function Hire() {
-
+export default function Downloads() {
   return (
-    <Layout title="Downloads">
-      <p>
-        <b>Coming soon!</b> I'll be uploading various items here for download - some free, some paid. Will include anything from songs I've made, to spreadsheet templates, to eBooks I've written and much more!
-      </p>
+    <Layout title="Resources & Downloads">
+      <div className="panel">
+        <div className="panel-block">
+          <div className="control has-icons-left">
+            <input className="input is-primary" type="text" placeholder="Search" />
+            <FontAwesomeIcon
+              icon={faSearch}
+              size='xs'
+              className='icon is-left search-icon'
+            />
+          </div>
+          <div className="panel-block level">
+            <div className="level-item has-text-centered">
+              <label className="checkbox">
+                <input type="checkbox" checked />Paid
+              </label>
+            </div>
+            <div className="level-item has-text-centered">
+              <label className="checkbox">
+                <input type="checkbox" checked />Free
+              </label>
+            </div>
+          </div>
+        </div>
+        <p className="panel-tabs">
+          <a className="is-active download-link">All</a>
+          <a className="download-link">Word Docs</a>
+          <a className="download-link">Slide Decks</a>
+          <a className="download-link">eBooks</a>
+          <a className="download-link">Music</a>
+          <a className="download-link">Links</a>
+        </p>
+        <a className="panel-block is-active">
+          <span className="panel-icon">
+            <i className="fas fa-book" aria-hidden="true"></i>
+          </span>
+          bulma
+        </a>
+        <a className="panel-block">
+          <span className="panel-icon">
+            <i className="fas fa-book" aria-hidden="true"></i>
+          </span>
+          marksheet
+        </a>
+        <a className="panel-block">
+          <span className="panel-icon">
+            <i className="fas fa-book" aria-hidden="true"></i>
+          </span>
+          minireset.css
+        </a>
+        <a className="panel-block">
+          <span className="panel-icon">
+            <i className="fas fa-book" aria-hidden="true"></i>
+          </span>
+          jgthms.github.io
+        </a>
+        <a className="panel-block">
+          <span className="panel-icon">
+            <i className="fas fa-code-branch" aria-hidden="true"></i>
+          </span>
+          daniellowtw/infboard
+        </a>
+        <a className="panel-block">
+          <span className="panel-icon">
+            <i className="fas fa-code-branch" aria-hidden="true"></i>
+          </span>
+          mojs
+        </a>
+      </div>
     </Layout>
   );
 };
