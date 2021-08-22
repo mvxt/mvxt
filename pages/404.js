791f84382b053ac56172821b52df6ed2c0bd26fa
Michael Vinh Xuan Thanh <meta.orphic@gmail.com>
2021-08-21 23:25:09 -0600

    Fix 404 page

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout';
 export default function NotFound() {
   return (
     <Layout title="404 Not Found">
-      <p>The page you're looking for was not found. If you think this is a mistake, please reach out to me. My contact info is on the <a href="/about">About page</a>.</p>
+      <p>The page you're looking for was not found. If you think this is a mistake, please reach out to me.</p>
     </Layout>
   );
 };
