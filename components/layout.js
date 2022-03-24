ba9b28c6658c4ca32cdcde3b5cc3e4cc01ebd0d3
Michael Vinh Xuan Thanh <meta.orphic@gmail.com>
2022-03-24 00:45:17 -0600

    Remove hero header, no longer needed

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 
-import Header from './header';
 import Footer from './footer';
 import Navbar from './navbar';
 
@@ -16,7 +15,6 @@ export default function Layout({ children, ...props }) {
       <div id="wrapper" className="px-8">
         <div className="columns">
           <div className="column auto">
-            <Header/>
             <Navbar/>
             <article className="article">
               <h1 className="is-size-3 has-text-centered">
