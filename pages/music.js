193df8ef2b2f545fd256fbe783e69568d0d2d1ff
Michael Vinh Xuan Thanh <meta.orphic@gmail.com>
2022-03-31 00:09:31 -0600

    Add minor space between soundcloud modules

--- pages/music.js
+++ pages/music.js
@@ -45,6 +45,7 @@ export default function Music() {
         allow="autoplay"
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/746874703&color=%23583e36&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
       </iframe>
+      <br/>
       <iframe
         width="100%"
         height="300"
